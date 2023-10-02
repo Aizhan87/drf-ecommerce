20148a4f1148a63a2f79b48e6f4498b76b9cf1bf
Aizhan87 <moonsoul87@hotmail.com>
2023-10-01 20:43:53 -0600

    added redux

--- backend/base/models.py
+++ backend/base/models.py
@@ -10,7 +10,7 @@ class Product(models.Model):
     brand = models.CharField(max_length=200, null=True, blank=True)
     category = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    raiting = models.DecimalField(
+    rating = models.DecimalField(
         max_digits=7, decimal_places=2, null=True, blank=True)
     numReviews = models.IntegerField(null=True, blank=True, default=0)
     price = models.DecimalField(
@@ -27,7 +27,7 @@ class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
-    raiting = models.IntegerField(null=True, blank=True, default=0)
+    rating = models.IntegerField(null=True, blank=True, default=0)
     comment = models.TextField(null=True, blank=True)
     _id = models.AutoField(primary_key=True, editable=False)
 
