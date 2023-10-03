5dc0c8c9f2b3ddaa4b476f28b78d81ceb9dc6c31
Aizhan87 <moonsoul87@hotmail.com>
2023-10-03 13:43:30 -0600

    added /api/users/profile/update/ endpoint

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -27,6 +27,8 @@ class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 
+
+
 @api_view(['POST'])
 def registerUser(request):
     data = request.data
@@ -42,6 +44,27 @@ def registerUser(request):
     except:
         message = {'detail': 'User with this email already exists'}
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
+  
+  
+    
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUserProfile(request):
+    user = request.user
+    serializer = UserSerializerWithToken(user, many=False)
+    data = request.data
+    
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+    if data['password'] != '':
+        user.password = make_password(data['password'])
+        
+    user.save()
+    return Response(serializer.data)
+
+
 
 
 @api_view(['GET'])
@@ -50,6 +73,8 @@ def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
+
+
 
 
 @api_view(['GET'])
