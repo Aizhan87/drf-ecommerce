b9b2b3b1367c1dfb3d1a74abeaaf9312ad2344a2
Aizhan87 <moonsoul87@hotmail.com>
2023-10-04 11:51:23 -0600

    shipping screen, checkout steps, payment screen/method, place order

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,9 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 
 function App() {
@@ -21,6 +24,9 @@ function App() {
             <Route path='/login' Component={LoginScreen} />
             <Route path='/register' Component={RegisterScreen} />
             <Route path='/profile' Component={ProfileScreen} />
+            <Route path='/shipping' Component={ShippingScreen} />
+            <Route path='/payment' Component={PaymentScreen} />
+            <Route path='/placeorder' Component={PlaceOrderScreen} />
             <Route path='/product/:id' Component={ProductScreen}/>
             <Route path='/cart/:id?' Component={CartScreen}/>
           </Routes>
