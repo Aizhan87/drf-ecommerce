3b78587fc44ef2f9c85964bbcb587799f1c4da94
Aizhan87 <moonsoul87@hotmail.com>
2023-10-10 15:16:28 -0600

    create review endpoint

--- backend/base/models.py
+++ backend/base/models.py
@@ -28,6 +28,7 @@ class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
     rating = models.IntegerField(null=True, blank=True, default=0)
+    createdAt = models.DateTimeField(auto_now_add=True)
     comment = models.TextField(null=True, blank=True)
     _id = models.AutoField(primary_key=True, editable=False)
 
